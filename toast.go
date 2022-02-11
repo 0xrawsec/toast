1dc8dff5960a22e5007cf4684f9499de9c371e71
Quentin JEROME <qjerome@users.noreply.github.com>
2022-02-11 09:41:49 +0100

    Good line number of message

--- toast.go
+++ toast.go
@@ -2,6 +2,8 @@ package toast
 
 import (
 	"fmt"
+	"path/filepath"
+	"runtime"
 	"strings"
 	"testing"
 )
@@ -23,6 +25,18 @@ func mkfmt(i ...interface{}) string {
 }
 
 func msg(or string, i ...interface{}) string {
+	var prefix string
+	_, filename, line, ok := runtime.Caller(2)
+
+	if ok {
+		prefix = fmt.Sprintf("\r    %s:%d:", filepath.Base(filename), line)
+		i = append([]interface{}{prefix}, i...)
+		if len(i) > 1 {
+			return fmt.Sprintf(mkfmt(i...), i...)
+		}
+		return strings.Join([]string{prefix, or}, " ")
+	}
+
 	if len(i) > 0 {
 		return fmt.Sprintf(mkfmt(i...), i...)
 	}
@@ -37,7 +51,7 @@ func AssertOrPanic(condition bool, i ...interface{}) {
 
 func Assert(t *testing.T, condition bool, i ...interface{}) {
 	if !condition {
-		t.Error(msg(assertFailMsg, i...))
+		t.Log(msg(assertFailMsg, i...))
 		if FailFast {
 			t.FailNow()
 		}
@@ -47,7 +61,14 @@ func Assert(t *testing.T, condition bool, i ...interface{}) {
 func ShouldPanic(t *testing.T, f func(), i ...interface{}) {
 	defer func() { recover() }()
 	f()
-	t.Error(msg(assertFailMsg, i...))
+	t.Log(msg(assertFailMsg, i...))
+	if FailFast {
+		t.FailNow()
+	}
+}
+
+func Error(t *testing.T, i ...interface{}) {
+	t.Log(msg("", i...))
 	if FailFast {
 		t.FailNow()
 	}
